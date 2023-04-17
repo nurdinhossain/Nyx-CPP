89edb2b496c264cdeecc85bbcce2b698318028e0
Nurdin <nurdinhossain@gmail.com>
2023-04-17 19:09:12 -0400

    Re-enable check extensions

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ using namespace std;
 int main()
 {
 	// r4k1r/1pP1npp1/1Pb4p/p6q/Q1p2p1b/B1P2N2/P2N2P1/R2K3R w - - 0 1 is a wonderful FEN string to test with
+	// rn3rk1/pbppq1pp/1p2pb2/4N2Q/3PN3/3B4/PPP2PPP/R3K2R w KQ - 7 11 is a mate in 7 puzzle
 
 	// get user input
 	string input;
